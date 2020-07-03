7c82495b8c4a2cc6d55f0d4b75a35b5f59e1d7a4
Maynor Taisigue <mtaisigue@gmail.com>
2020-07-03 01:00:42 -0700

    vonage sms verify cert

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# vonage-sms-verify-cert-project
+# Implementing Certification Project for SMS & Verify API certification
+
+This repo contains the code for the [certification project](https://vonage-workshop.nexmodev.com/verify/certification/certification-project/) on using the Nexmo Verify ans SMS API. It is written in Node.js using the Express framework.
+
+## Get the code
+Follow these steps to get your own version of this up and running:
+
+```bash
+git clone https://github.com/mtaisigue/vonage-sms-verify-cert-project.git
+cd vonage-sms-verify-cert-project && npm install
+```
+
+## Configuring the application
+Once installed, copy the `env-example` file to `.env` in the application's root directory. Enter your API key, secret and virtual number from the [Nexmo Developer Dashboard](https://dashboard.nexmo.com) and also a name for your application which will appear on the home page and also in the `from` field of any SMS sent via the Verify API.
+
+```
+NEXMO_API_KEY=YOUR NEXMO API KEY
+NEXMO_API_SECRET=YOUR NEXMO API SECRET
+NEXMO_BRAND_NAME=UP TO 11 ALPHANUMERIC CHARACTERS
+VIRTUAL_NUMBER=LVN
+```
+## Running the application
+You should then be able to run the app with `npm start`.
+
+Then on two different browser windows (if using the same browser one of these should be a private window) login as a "Driver" first on one browser and as a "Rider" in the other one.
+
+After login, as a user select a driver from the list and click on "Contact your driver". both should receive an SMS and can start texting each other using that sms thread.
---
+++ server.js
@@ -0,0 +1,141 @@
+require("dotenv").load();
+
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const SmsProxy = require("./SmsProxy");
+
+const app = express();
+const smsProxy = new SmsProxy();
+
+let users = Array();
+
+app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: "loadsofrandomstuff",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  /*
+        If there is a session for the user, the `index.html`
+        page will display the number that was used to log
+        in. If not, it will prompt them to log in.
+    */
+  if (!req.session.user) {
+    res.render("index", {
+      brand: smsProxy.brand,
+    });
+  } else {
+    res.render("index", {
+      number: req.session.user.number,
+      brand: smsProxy.brand,
+      drivers: users.filter((user) => user.role === "driver"),
+      role: req.session.user.role,
+    });
+  }
+});
+
+app.get("/login", (req, res) => {
+  // Display the login page
+  res.render("login");
+});
+
+app.post("/verify", async (req, res) => {
+  // Start the verification process
+  const user = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    number: req.body.number,
+    role: req.body.role,
+  };
+
+  try {
+    const result = await smsProxy.requestCode(user.number);
+    user.verifyRequestId = result;
+
+    // saves the user
+    users.push(user);
+    console.log(`request_id: ${user.verifyRequestId}`);
+    /* 
+        Redirect to page where the user can 
+        enter the code that they received
+     */
+    res.render("entercode", {
+      requestid: user.verifyRequestId,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("/check-code", async (req, res) => {
+  // Check the code provided by the user
+  const user = users.filter(
+    (user) => user.verifyRequestId === req.body.requestid
+  )[0];
+
+  try {
+    const result = await smsProxy.checkVerificationCode(
+      req.body.requestid,
+      req.body.code
+    );
+    if (result.status == 0) {
+      /* 
+                  User provided correct code,
+                  so create a session for that user
+              */
+      req.session.user = {
+        number: user.number,
+        role: user.role,
+      };
+    }
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/logout", (req, res) => {
+  users = users.filter((user) => user.number != req.session.user.number);
+  req.session.destroy();
+  res.redirect("/");
+});
+
+app.post("/chat", (req, res) => {
+  const user = req.body.user;
+  const driver = req.body.driver;
+
+  smsProxy.createChat(user, driver, (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.json(result);
+    }
+  });
+  res.send("You can text your driver replying to the text you just received!");
+});
+
+app.get("/webhooks/inbound-sms", (req, res) => {
+  const from = req.query.msisdn;
+  const to = req.query.to;
+  const text = req.query.text;
+
+  console.log("got message from", from);
+
+  // Route virtual number to real number
+  smsProxy.proxySms(from, text);
+
+  res.sendStatus(204);
+});
+
+const server = app.listen(3000, () => {
+  console.log(`Server running on port ${server.address().port}`);
+});
